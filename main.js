3d2513a5820fabaecec07e748dd3787852436427
luciocatilina <pablocandia395@gmail.com>
2023-04-23 21:47:47 -0300

    cambios de divisas implementado

--- main.js
+++ main.js
@@ -9,7 +9,6 @@ const productos = [
 ]
 
 
-
 let productosContenedor = document.getElementById('productos');
 
 let productosContenedorCarrito = document.getElementById('carritoAbierto');
@@ -26,10 +25,39 @@ let carritoLista = [];
 
 let carritoStorage = [];
 
+let boton_cambio_moneda = document.getElementById('boton_cambio_moneda')
+let sublista_monedas = document.getElementById('sublista_monedas');
+
 /*API MONEDAS*/
 const DateTime = luxon.DateTime;
 let x = DateTime.now().toFormat('yyyy-MM-dd')
-listaMonedas = ['usd', 'eur', 'cny', 'rub', 'brl']
+let listaMonedas = ['usd', 'eur', 'cny', 'rub', 'brl']
+
+
+let listadoTotalMonedas = ['ars'].concat(listaMonedas);
+
+// localStorage.setItem('moneda', listadoTotalMonedas[0])
+// localStorage.setItem('valorMoneda', 1)
+
+let valorMoneda = Number(localStorage.getItem('valorMoneda'));
+
+let tipoMoneda = localStorage.getItem('moneda');
+
+(valorMoneda || tipoMoneda) || (valorMoneda=1, tipoMoneda='ars')
+console.log(valorMoneda, tipoMoneda)
+
+boton_cambio_moneda.innerHTML = `<i class="fa-solid fa-dollar-sign" id="${tipoMoneda}" >${tipoMoneda.toUpperCase()}</i>`;
+
+listadoTotalMonedas.forEach((moneda) => {
+    if (boton_cambio_moneda.innerText !== `${moneda.toLocaleUpperCase()}`) {
+        sublista_monedas.innerHTML+= `<li id=${moneda} class='boton_moneda'>${moneda.toLocaleUpperCase()}</li>`
+    }
+})
+boton_cambio_moneda.append(sublista_monedas)
+listadoTotalMonedas.forEach((moneda) => {
+    let btnMoneda = document.getElementById(`${moneda}`)
+    btnMoneda.addEventListener('click', ()=> botonCambiarMoneda(moneda))
+});
 
 listaMonedas.forEach((moneda) => {
     const traerMoneda = async () => {
@@ -40,7 +68,7 @@ listaMonedas.forEach((moneda) => {
             i = data.ars.toFixed(2)       
             monedaContainer = document.createElement('DIV')
             apiContainer.append(monedaContainer)
-            monedaContainer.innerHTML = `${moneda.toUpperCase()} = $${i}ARS`
+            monedaContainer.innerHTML = `${moneda.toUpperCase()} = $<b id='${moneda+moneda}'>${i}</b> ARS`
         } catch (error) {
             console.log(error)
             }
@@ -64,18 +92,18 @@ const traerProductos = () => {
     })
 }
 
-traerProductos()
+traerProductos(valorMoneda, tipoMoneda)
     .then((response) => {
         response.forEach(producto => {
             let productoContenedor = document.createElement('div');
             productosContenedor.append(productoContenedor);
             productoContenedor.classList.add('producto');
             productoContenedor.innerHTML = `
-                <img src="${producto.img}" alt="">
+                <img src="${producto.img}" alt="${producto.nombre}">
                 <div class="producto__descripcion">
                     <h3>Id: ${producto.id}</h3> 
                     <h3>${producto.nombre}</h3> 
-                    <p class="precio">$${producto.precio}</p>
+                    <p class="precio precio_listado">$${(producto.precio/valorMoneda).toFixed(2)} ${tipoMoneda.toLocaleUpperCase()}</p>
                 </div>
                 <button class="carrito_boton" id="carrito_boton${producto.id}">Agregar al carrito</button>
                 `;
@@ -99,6 +127,7 @@ carritoFinal.forEach(p => {listadoIds.push(p.id)})
 
 carrito.innerHTML = `<i class="fa-solid fa-cart-shopping"></i> (${contadorProductos})`;
 
+
 /*AGREGAR PRODUCTO AL CARRITO */
 const agregarProducto = (id) => {
     Toastify({
@@ -118,27 +147,45 @@ const agregarProducto = (id) => {
     localStorage.setItem('cant', contadorProductos)
     localStorage.setItem('carrito', JSON.stringify(carritoFinal))
     carrito.innerHTML = `<i class="fa-solid fa-cart-shopping"></i> (${contadorProductos})`;
-    carrito.addEventListener('click', aperturaCarrito)
+    carrito.addEventListener('click', function () {aperturaCarrito(valorMoneda, tipoMoneda)});
+
+
 };
 
-
+let totalCarrito = 0
 /*APERTURA CARRITO*/
-const aperturaCarrito = () => {
+const aperturaCarrito = (valorMoneda, tipoMoneda) => {
+    carritoFinal.forEach(p => {
+        totalCarrito += (p.precio/valorMoneda)*p.cantidad
+    })
     carritoCaja.classList.add('mostrar_caja', 'productos');
     carritoCaja.innerHTML = `
         <div class='carrito_caja_titulo'>
             <h3>Carrito de compras</h3>
             <button id='botonCerrarCarrito' class="carrito_boton_masmenos"><i class="fa-solid fa-x fa-2x"></i></button>
         </div>
+        <div class="carrito_total">
+            <p>Total: $${totalCarrito.toFixed(2)}</p>
+        </div>
         <div class='carrito_caja_pie'>
             <button id='botonLimpiarCarrito'>Vaciar carrito</button>
+            <button id='confirmar_compra'>Comprar</button>
         </div>
     `;
 
     tienda.classList.add('ocultar_tienda')
 
+    if(valorMoneda && tipoMoneda) {
+        valorMoneda = valorMoneda;
+        tipoMoneda = tipoMoneda;
+    }else {
+        valorMoneda = 1;
+        tipoMoneda = 'ars'
+    }
+    
     carritoFinal.forEach(producto => {
-        let productoCarrito = document.createElement('div');
+        
+        productoCarrito = document.createElement('div');
         carritoCaja.append(productoCarrito);
         productoCarrito.classList.add('producto');
         productoCarrito.innerHTML = `
@@ -146,7 +193,7 @@ const aperturaCarrito = () => {
             <div class="producto__descripcion">
                 <h3>Cantidad: ${producto.cantidad}</h3> 
                 <h3>${producto.nombre}</h3> 
-                <p class="precio">$${producto.precio*producto.cantidad}</p>
+                <p class="precio precio_carrito_listado">$${((producto.precio/valorMoneda)*producto.cantidad).toFixed(2)} ${tipoMoneda.toLocaleUpperCase()}</p>
             </div>
             <div class='masmenos'>
                 <button class="carrito_boton_masmenos" id="carrito_boton_sumar${producto.id}"><i class="fa-solid fa-plus fa-2x"></i></button>
@@ -154,19 +201,27 @@ const aperturaCarrito = () => {
             </div>  
             `;
 
-        let botonRestar = document.getElementById(`carrito_boton_restar${producto.id}`)
-        let botonSumar = document.getElementById(`carrito_boton_sumar${producto.id}`)
+        botonRestar = document.getElementById(`carrito_boton_restar${producto.id}`)
+        botonSumar = document.getElementById(`carrito_boton_sumar${producto.id}`)
         botonRestar.addEventListener('click', ()=> restarProducto(producto.id))
         botonSumar.addEventListener('click', ()=> sumarProducto(producto.id))
     })
-    let botonCerrarCarrito = document.getElementById('botonCerrarCarrito');
+
+    botonCerrarCarrito = document.getElementById('botonCerrarCarrito');
     botonCerrarCarrito.addEventListener('click', () => cerrarCarrito());
 
-    let botonLimpiarCarrito = document.getElementById('botonLimpiarCarrito');
+    botonLimpiarCarrito = document.getElementById('botonLimpiarCarrito');
     botonLimpiarCarrito.classList.add('carrito_boton')
-    botonLimpiarCarrito.addEventListener('click', () => limpiarCarrito())
+    botonLimpiarCarrito.addEventListener('click', () => limpiarCarrito('¿Desea vaciar el carrito?'))
 
+    confirmar_compra = document.getElementById('confirmar_compra');
+    confirmar_compra.classList.add('carrito_boton')
+    confirmar_compra.addEventListener('click', () => limpiarCarrito('¿Desea confirmar la compra?'))
+    
 };
+
+
+
 /*RESTAR PRODUCTO*/
 const restarProducto = (id) => {
     let productoCarro = carritoFinal.find((item) => item.id === id);
@@ -178,7 +233,8 @@ const restarProducto = (id) => {
     if (productoCarro.cantidad == 0) {
         eliminarProducto(id)
     }
-    aperturaCarrito();
+    aperturaCarrito(valorMoneda, tipoMoneda);
+    
 }
 /*SUMAR PRODUCTO*/
 const sumarProducto = (id) => {
@@ -188,7 +244,8 @@ const sumarProducto = (id) => {
     localStorage.setItem('cant', contadorProductos)
     localStorage.setItem('carrito', JSON.stringify(carritoFinal))
     carrito.innerHTML = `<i class="fa-solid fa-cart-shopping"></i> (${contadorProductos})`;
-    aperturaCarrito();
+    aperturaCarrito(valorMoneda, tipoMoneda);
+    
 }
 /*ELIMINACION DE PRODUCTO DEL CARRTIO */
 const eliminarProducto = (id) => {
@@ -202,7 +259,7 @@ const eliminarProducto = (id) => {
     }
     localStorage.setItem('carrito', JSON.stringify(carritoFinal))
     carrito.innerHTML = `<i class="fa-solid fa-cart-shopping"></i> (${contadorProductos})`;
-    aperturaCarrito();
+    aperturaCarrito(valorMoneda, tipoMoneda);
 };
 
 /*CERRAR CARRITO*/
@@ -213,14 +270,14 @@ function cerrarCarrito() {
 }
 
 /*LIMPIAR CARRITO*/
-function limpiarCarrito() {
+function limpiarCarrito(texto) {
     localStorage.clear();
-    alertOk('¿Desea vaciar el carrito?');
+    alertOk(texto);
 }
 
-/*SI EL CARRITO ESTA VACIO, NO TIENE EVENTO */
-(carritoFinal.length == 0) || carrito.addEventListener('click', aperturaCarrito);
 
+/*SI EL CARRITO ESTA VACIO, NO TIENE EVENTO */
+(carritoFinal.length == 0) || carrito.addEventListener('click', function () {aperturaCarrito(valorMoneda, tipoMoneda);}); 
 
 
 /*SWEET ALERT - VACIAR CARRITO*/
@@ -236,8 +293,8 @@ function alertOk (texto) {
       }).then((result) => {
         if (result.isConfirmed) {
           Swal.fire(
-            '¡Borrado!',
-            '¡Has vaciado el carrito!',
+            '¡Completado!',
+            '',
             'success',
             setTimeout( () => {
                 location. reload();
@@ -246,3 +303,182 @@ function alertOk (texto) {
         }
       })  
 }
+
+
+
+/*BOTONES PARA EL CAMBIO DE MONEDA */
+let elementoRemover
+let btnMoneda
+
+const botonCambiarMoneda = (coin) => {
+    elementoRemover = document.getElementById(`${coin}`)
+    elementoRemover.remove()
+
+    boton_cambio_moneda.innerHTML = `<i class="fa-solid fa-dollar-sign" id="${coin}">${coin.toString().toUpperCase()} </i>`
+    let indice = listadoTotalMonedas.indexOf(coin);
+    
+    let elemento_a_ir_primero = listadoTotalMonedas.splice(indice, 1);
+    listadoTotalMonedas.unshift(elemento_a_ir_primero);
+
+    sublista_monedas.innerHTML = ''
+
+    listadoTotalMonedas.forEach((moneda) => {
+        if (boton_cambio_moneda.innerText !== `${moneda.toString().toLocaleUpperCase()}`) {
+            sublista_monedas.innerHTML+= `<li id=${moneda} class='boton_moneda'>${moneda.toString().toLocaleUpperCase()}</li>`
+            
+        }
+        
+    })
+    boton_cambio_moneda.appendChild(sublista_monedas)
+
+    listadoTotalMonedas.forEach((moneda) => {
+        if (boton_cambio_moneda.innerText !== `${moneda.toString().toLocaleUpperCase()}`) {
+            btnMoneda = document.getElementById(`${moneda}`)
+            btnMoneda.addEventListener('click', ()=> botonCambiarMoneda(moneda))
+        }
+        
+    })
+
+    cambiarMoneda(coin);
+}
+
+
+/*CAMBIO DE MONEDA */
+
+// localStorage.setItem('moneda', listadoTotalMonedas[0])
+// localStorage.setItem('valorMoneda', 1)
+
+const cambiarMoneda = (coin) => {
+
+    listadoPrecios = document.querySelectorAll('.precio_listado'); //lista de nodos de los precios
+    listadoPreciosCarritos = document.querySelectorAll('.precio_carrito_listado'); //lista de nodos de los precios del carrito
+
+
+    /*DOLAR - PESO */
+    let pesoUsd = document.getElementById("usdusd");
+    let dolarAPeso = pesoUsd.innerText;
+    dolarAPeso = Number(dolarAPeso);
+    const listaUsd = []
+
+    /*EUR - PESO */
+    let pesoEur = document.getElementById('eureur');
+    let euroAPeso = pesoEur.innerText;
+    euroAPeso = Number(euroAPeso);
+    const listaEur = []
+
+    /*CNY - PESO */
+    let pesoCny = document.getElementById('cnycny');
+    let cnyAPeso = pesoCny.innerText;
+    cnyAPeso = Number(cnyAPeso);
+    const listaCny = []
+
+    /*RUB - PESO */
+    let pesoRub = document.getElementById('rubrub');
+    let rubAPeso = pesoRub.innerText;
+    rubAPeso = Number(rubAPeso);
+    const listaRub = []
+
+    /*BRL - PESO */
+    let pesoBrl = document.getElementById('brlbrl');
+    let brlAPeso = pesoBrl.innerText;
+    brlAPeso = Number(brlAPeso);
+    const listaBrl = []
+    
+    if (coin == 'ars') {
+        productos.forEach((p, indice) => {
+            listadoPrecios[indice].innerText = `$${p.precio} ARS`
+        })
+        // valorMoneda = 1;
+        // tipoMoneda = 'ARS'
+        location.reload() //para que se actualice el precio del listado de productos en el carrito
+        localStorage.setItem('moneda', coin)
+        localStorage.setItem('valorMoneda', 1)
+        console.log(valorMoneda, tipoMoneda)
+
+
+    }else if (coin == 'usd') {
+        
+        productos.forEach((p) => {
+            precioUsdFinal = p.precio/dolarAPeso
+            listaUsd.push(precioUsdFinal)
+        })
+        cambiarDom(listaUsd ,coin)
+        // valorMoneda = dolarAPeso;
+        // tipoMoneda = 'USD'
+        location.reload() //para que se actualice el precio del listado de productos en el carrito
+        localStorage.setItem('moneda', coin)
+        localStorage.setItem('valorMoneda', dolarAPeso)
+        console.log(valorMoneda, tipoMoneda)
+        
+        
+    }else if (coin == 'eur') {
+        
+        productos.forEach((p) => {
+            precioEurFinal = p.precio/euroAPeso
+            listaEur.push(precioEurFinal)
+        })
+        cambiarDom(listaEur ,coin)
+        // valorMoneda = euroAPeso;
+        // tipoMoneda = 'EUR'
+        location.reload() //para que se actualice el precio del listado de productos en el carrito
+        localStorage.setItem('moneda', coin)
+        localStorage.setItem('valorMoneda', euroAPeso)
+        console.log(valorMoneda, tipoMoneda)
+        
+    }else if (coin == 'cny') {
+        
+        productos.forEach((p) => {
+            precioCnyFinal = p.precio/cnyAPeso
+            listaCny.push(precioCnyFinal)
+        })
+        cambiarDom(listaCny ,coin)
+        // valorMoneda = cnyAPeso;
+        // tipoMoneda = 'CNY'
+        location.reload() //para que se actualice el precio del listado de productos en el carrito
+        localStorage.setItem('moneda', coin)
+        localStorage.setItem('valorMoneda', cnyAPeso)
+        console.log(valorMoneda, tipoMoneda)
+        
+    }else if (coin == 'rub') {
+        
+        productos.forEach((p) => {
+            precioRubFinal = p.precio/rubAPeso
+            listaRub.push(precioRubFinal)
+        })
+        cambiarDom(listaRub ,coin)
+        // valorMoneda = rubAPeso;
+        // tipoMoneda = 'RUB'
+        location.reload() //para que se actualice el precio del listado de productos en el carrito
+        localStorage.setItem('moneda', coin)
+        localStorage.setItem('valorMoneda', rubAPeso)
+        console.log(valorMoneda, tipoMoneda)
+        
+    }else if (coin == 'brl') {
+        
+        productos.forEach((p) => {
+            precioBrlFinal = p.precio/brlAPeso
+            listaBrl.push(precioBrlFinal)
+        })
+        cambiarDom(listaBrl ,coin)
+        // valorMoneda = brlAPeso;
+        // tipoMoneda = 'BRL'
+        location.reload() //para que se actualice el precio del listado de productos en el carrito
+        localStorage.setItem('moneda', coin)
+        localStorage.setItem('valorMoneda', brlAPeso)
+        console.log(valorMoneda, tipoMoneda)
+        
+    }
+
+}
+
+
+/*Asignar cambio de moneda al dom */
+const cambiarDom = (lista, tipoMoneda) => {
+    lista.forEach((valor, indice) => {
+        listadoPrecios[indice].innerText = `$${valor.toFixed(2)} ${tipoMoneda.toString().toLocaleUpperCase()}`
+    }) 
+}
+
+
+
+
